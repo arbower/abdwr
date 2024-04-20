6790d9e3bc72ee9b5c0330b0541cffa0666e3a80
Andrew Bower <a.robert.bower@gmail.com>
2024-04-19 23:25:43 -0700

    Created and started chapter 1 in abdwr book

---
+++ scripts/c01_abdwr_lahman_intro.R
@@ -0,0 +1,24 @@
+# Description: Chapter 1: Analyzing Baseball Data with R
+
+# Author: Andrew R. Bower
+# Version: 19 April, 2024
+
+# Packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("tidyverse", "assertthat", "babynames", "broom", "compare", "DBI", "dbplyr",
+               "devtools", "fivethirtyeight", "flexdashboard", "fs", "ggrepel", "googlesheets4",
+               "here", "hexbin", "httr", "janitor", "jsonlite", "knitr", "leaflet", "nycflights13",
+               "RPostgreSQL", "rvest", "scales", "tidycensus", "usethis", "vroom", "yaml" )
+
+# Parameters
+  # Input Data
+
+  # Output Data
+
+  # Load Data
+
+#===============================================================================
+
+# Additional Packages
+pacman::p_load(Lahman)
+
